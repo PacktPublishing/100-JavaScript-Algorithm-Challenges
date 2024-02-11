3efbdbade7095094ad85d81deb16ef9c16457037
listenToRipley <natalie.m.kendrick@gmail.com>
2024-02-10 20:33:30 -0500

    refactored

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,18 +1,27 @@
 export const addBorder = (pic: string[]): string[] => {
-  const lengthOfWall: number = pic[0].length + 2;
-  let wall = '';
-  for (let i = 0; i < lengthOfWall; i++) {
-    wall = wall.concat('*')
-  }
+  // const lengthOfWall: number = pic[0].length + 2;
+  // let wall = '';
+  // for (let i = 0; i < lengthOfWall; i++) {
+  //   wall = wall.concat('*')
+  // }
 
-  pic.unshift(wall);
-  pic.push(wall);
+  // pic.unshift(wall);
+  // pic.push(wall);
 
-  for (let i = 1; i < pic.length - 1; i++) {
-    pic[i] = '*'.concat(pic[i], '*');
-  }
+  // for (let i = 1; i < pic.length - 1; i++) {
+  //   pic[i] = '*'.concat(pic[i], '*');
+  // }
 
-  return pic;
+  // return pic;
+  const wall = '*'.repeat(pic[0].length + 2);
+  let framed = [...pic]; //make deep copy.
+  
+  framed.forEach((item, idx) => framed[idx] ='*'.concat(item, '*'))
+  
+  framed.unshift(wall);
+  framed.push(wall);
+  
+  return framed;
 };
 
 console.log(addBorder(['abc','ded']));
